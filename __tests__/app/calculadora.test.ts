ad9feb24649059481a8feae675a1ea4ec11ed3ba
Daniel Gutierrez <dgutierrez@dgutierrezmac.local>
2025-04-24 19:41:11 -0400

    respaldos

--- __tests__/app/calculadora.test.ts
+++ __tests__/app/calculadora.test.ts
@@ -1,5 +1,5 @@
 import { describe, test } from "@jest/globals";
-import { restar, suma } from "../../src/app/calculadora";
+import { suma, restar, multiplicacion, division, potencia, factorial, operar  } from "../../src/app/calculadora";
 
 describe("Suite de test de calculadora", () => {
 
@@ -40,4 +40,69 @@ describe("Suite de test de calculadora", () => {
         a = undefined
         expect(() => { restar(a, 1) }).toThrow("No se puede restar indefinidos");
     })
+
+     test("probar multiplicacion", () => {
+
+        let a: any = "1";
+        expect(multiplicacion(1,1)).toBe(1);
+        expect(multiplicacion(a,1)).toBeNaN();
+
+        a = undefined;
+        expect(()=> {multiplicacion(a,1)}).toThrow("No se puede multiplicar indefinidos")
+
+    })
+
+    test("probar division", () => {
+
+        let a: any = "1";
+        expect(division(1,1)).toBe(1);
+        expect(division(a,1)).toBeNaN();
+
+        a = undefined;
+        expect(()=> {division(a,1)}).toThrow("No se puede dividir indefinidos")
+
+        expect(()=> {division(10,0)}).toThrow("No se puede dividir por 0")
+
+    })
+
+    test("probar potencia", () => {
+
+        let a: any = "1";
+        expect(potencia(1,1)).toBe(1);
+        expect(potencia(a,1)).toBeNaN();
+
+        a = undefined;
+        expect(()=> {potencia(a,1)}).toThrow("No se puede hacer potencia con indefinidos")
+
+       
+
+    })
+
+    test("probar factorial", () => {
+
+        let a: any = "1";
+        expect(factorial(1)).toBe(1);
+        expect(factorial(a)).toBeNaN();
+
+        a = undefined;
+        expect(()=> {factorial(a)}).toThrow("No se puede hacer factoriales con indefinidos")
+
+       
+
+    })
+
+    test("probar operar", () => {
+
+        let a: any = "1";
+        expect(operar("suma",1,1)).toBe(2);
+        expect(operar("resta",1,1)).toBe(0);
+        expect(operar("multiplicacion",1,1)).toBe(1);
+        expect(operar("division",1,1)).toBe(1);
+        expect(operar("potencia",1,1)).toBe(1); 
+        expect(operar("factorial",5,1)).toBe(120);
+        
+
+    })
+
+  
 });

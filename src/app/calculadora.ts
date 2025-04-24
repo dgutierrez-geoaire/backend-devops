90a1ccdaabc4ee00281c46de429a2156800a12a2
Daniel Gutierrez <dgutierrez@dgutierrezmac.local>
2025-04-24 16:48:48 -0400

    avance curso

--- src/app/calculadora.ts
+++ src/app/calculadora.ts
@@ -3,6 +3,14 @@ function operar(operacion: string = "", a: number, b: number) {
         return suma(a, b);
     } else if (operacion === 'resta') {
         return restar(a, b);
+    }else if (operacion === 'multiplicacion') {
+        return multiplicacion(a, b);
+    }else if (operacion === 'division') {
+        return division(a, b);
+    }else if (operacion === 'potencia') {
+        return potencia(a, b);
+    }else if (operacion === 'factorial') {
+        return factorial(a);
     }
 }
 
@@ -30,4 +38,64 @@ function restar(a: number, b: number) {
     return a - b;
 }
 
-export { suma, operar, restar };
+function multiplicacion(a: number, b: number) {
+    if (a === undefined || b === undefined) {
+
+        throw new Error("No se puede multiplicar indefinidos");
+    }
+    if (typeof a !== 'number' || typeof b !== 'number') {
+        return NaN;
+    }
+    return a * b;
+}
+
+function division(a: number, b: number) {
+    if (a === undefined || b === undefined) {
+
+        throw new Error("No se puede dividir indefinidos");
+    }
+    if (typeof a !== 'number' || typeof b !== 'number') {
+        return NaN;
+    }
+
+    if (b == 0){
+        throw new Error("No se puede dividir por 0");
+    }
+
+    return a / b;
+}
+
+function potencia(a: number, b: number) {
+    if (a === undefined || b === undefined) {
+
+        throw new Error("No se puede hacer potencia con indefinidos");
+    }
+    if (typeof a !== 'number' || typeof b !== 'number') {
+        return NaN;
+    }
+
+   
+
+    return a ** b;
+}
+
+function factorial(a: number) {
+    if (a === undefined) {
+
+        throw new Error("No se puede hacer factoriales con indefinidos");
+    }
+    if (typeof a !== 'number') {
+        return NaN;
+    }
+    var num = 1;
+    for (var i = 1; i<=a  ; i++) {
+         
+        num = num * i; 
+        
+       
+      }
+
+    return num;
+}
+
+export { suma, operar, restar, multiplicacion, division, potencia, factorial};
